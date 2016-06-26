d3cdbbbc41a3b815c12604aec7c6ded801e774d4
Gabriel Rubens <grsabreu@gmail.com>
2016-06-26 12:15:57 -0300

    Create new bundle

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ read update
 npm run bundle
 git commit -am "Create new bundle"
 
-TMP_DIR=/tmp/saito-tempo-piaui/
+TMP_DIR=/tmp/saito-tempo-piaui
 mkdir $TMP_DIR
 cp ./src/bundle.js $TMP_DIR
 cp ./src/index.html $TMP_DIR
@@ -20,3 +20,5 @@ git commit -m "$update"
 echo "\033[1;31mPushing new site to GitHub repo\n\033[0m"
 git push origin master
 git checkout develop
+
+rm -rf $TMP_DIR
