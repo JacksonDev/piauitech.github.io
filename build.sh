b573e9673844856441ab15bed35c24c8fb398bb6
Gabriel Rubens <grsabreu@gmail.com>
2017-05-16 22:22:26 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,35 @@
+
 #!/bin/sh
+
+TMP_DIR=$(mktemp -d)
 
 echo "\033[1;33m What have you done to the site?\033[0m"
 read update
 
-npm run bundle
+# Create bundle
+npm run build
 git commit -am "Create new bundle"
 
-TMP_DIR=/tmp/saito-tempo-piaui
-mkdir $TMP_DIR
-cp ./src/bundle.js $TMP_DIR
-cp ./src/index.html $TMP_DIR
+# Copy build to temp folder
+cp -r ./build/ $TMP_DIR/build
+BUILD_FILES=`ls ./build`
 
+# Wipe out current site
 git checkout master
+git rm -rf .
 
-git rm -rf *
-mv $TMP_DIR/**/* ./ -f
-git add --all
+# Move new bundle to master
+mv $TMP_DIR/build/** ./ -f
+
+# Create CNAME
+echo "piaui.tech" > CNAME
+git add CNAME
+git add $BUILD_FILES
+
+# Commit and push changes
 git commit -m "$update"
 echo "\033[1;31mPushing new site to GitHub repo\n\033[0m"
-git push origin master
-git checkout develop
+git push origin master --force
 
-rm -rf $TMP_DIR
+# Get back to develop
+git checkout develop
